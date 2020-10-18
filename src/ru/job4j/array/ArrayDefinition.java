88b318d558ad37818d44a6d8615bcefb72a388d5
sergey <s-krasssavin@mail.ru>
2020-10-18 21:55:01 +0300

    6.0.1.1. Размер массива [#379399]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,8 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        System.out.println("length: " + ages.length);
+        System.out.println("length: " + surnames.length);
+        System.out.println("length: " + prices.length);
     }
 }
