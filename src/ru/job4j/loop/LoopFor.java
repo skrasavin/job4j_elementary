523eee3160f11f8c49da05d0e5c48dc2988bcc76
sergey <s-krasssavin@mail.ru>
2020-10-21 01:05:51 +0300

    6.4.1.1. Обход массива с последнего элемента [#379401]

--- src/ru/job4j/loop/LoopFor.java
+++ src/ru/job4j/loop/LoopFor.java
@@ -2,8 +2,10 @@ package ru.job4j.loop;
 
 public class LoopFor {
     public static void main(String[] args) {
-        for (int index = 5; index <= 10; index++) {
-            System.out.println(index);
+        for (int index = 1; index <= 10; index++) {
+            if (index % 2 == 0) {
+                System.out.println(index);
+            }
         }
     }
 }
