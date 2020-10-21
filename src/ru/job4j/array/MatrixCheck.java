a94ea6aa67aef39a8b28b2329101f9b0b297ddef
sergey <s-krasssavin@mail.ru>
2020-10-22 02:47:03 +0300

    6.7.4. Выигрышные комбинации в сокобан [#379378]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -32,4 +32,43 @@ public class MatrixCheck {
         }
         return result;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (char[] chars : board) {
+            for (int j = 0; j < board.length; j++) {
+                if (chars[j] == 'X') {
+                    result = monoVertical(board, j);
+                    if (result) {
+                        break;
+                    }
+                    result = monoHorizontal(board, j);
+                }
+            }
+        }
+        return result;
+    }
+/*
+    public static void main(String[] args) {
+        char[][] input1 = {
+                {' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' '},
+                {'X', 'X', 'X', 'X', 'X'},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+        };
+        boolean result = MatrixCheck.isWin(input1);
+        System.out.println(result);
+
+        char[][] input = {
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+        };
+        boolean result1 = MatrixCheck.isWin(input);
+        System.out.println(result1);
+    }
+ */
 }
