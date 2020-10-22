048dbbe7f797b1f1686886895f7c0a047d78541c
sergey <s-krasssavin@mail.ru>
2020-10-22 19:58:05 +0300

    6.7.4. Выигрышные комбинации в сокобан [#379378]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -26,9 +26,7 @@ public class MatrixCheck {
     public static char[] extractDiagonal(char[][] board) {
         char[] result = new char[board.length];
         for (int i = 0; i < board.length; i++) {
-            if (board[i][i] == 'X') {
-                result[i] = 'X';
-            }
+            result[i] = board[i][i];
         }
         return result;
     }
