1d1198cae740a7edb2bb84e1ca8cebfd6109f970
sergey <s-krasssavin@mail.ru>
2020-10-26 22:43:49 +0300

    5. Аннотация @Override [#379581]

--- src/ru/job4j/inheritance/JSONReport.java
+++ src/ru/job4j/inheritance/JSONReport.java
@@ -2,11 +2,8 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
 
+    @Override
     public String generate(String name, String body) {
-        return   "{\n " + name + " : " + name + ",\n " + body + " : " + body + "\n}";
-    }
-
-    public String generateWithLineSeparator(String name, String body) {
         return   "{" + System.lineSeparator() + " " + name + " : " + name + ','
                 + System.lineSeparator() + " " + body + " : " + body
                 + System.lineSeparator() + "}";
