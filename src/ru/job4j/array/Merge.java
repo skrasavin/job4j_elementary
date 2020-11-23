10488a58cc50326b3b0dacf78234a4a1f9aaf01f
sergey <s-krasssavin@mail.ru>
2020-11-24 02:51:01 +0300

    1. Экзамен. Базовый синтаксис. [#379450]

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -6,14 +6,27 @@ public class Merge {
         int indL = 0;
         int indR = 0;
         int indRSL = 0;
+        if (left.length == 0) {
+            while (indR < right.length) {
+                System.out.println(indR);
+                rsl[indRSL++] = right[indR++];
+            }
+        }
+        if (right.length == 0) {
+            while (indL < left.length) {
+                rsl[indRSL++] = left[indL++];
+            }
+        }
         while (indL < left.length && indR < right.length) {
             rsl[indRSL++] = left[indL] < right[indR] ?  left[indL++] : right[indR++];
-        }
-        while (indL < left.length) {
-            rsl[indRSL++] = left[indL++];
-        }
-        while (indR < right.length) {
-            rsl[indRSL++] = right[indR++];
+            if (indRSL >= left.length || indRSL >= right.length) {
+                while (indL < left.length) {
+                    rsl[indRSL++] = left[indL++];
+                }
+                while (indR < right.length) {
+                    rsl[indRSL++] = right[indR++];
+                }
+            }
         }
         return rsl;
     }
