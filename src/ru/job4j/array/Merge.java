98599bd5743fa70bcb3972ec719166d86fb83f25
sergey <s-krasssavin@mail.ru>
2020-11-25 16:28:26 +0300

    1. Экзамен. Базовый синтаксис. [#379450]

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -1,33 +1,30 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int indL = 0;
-        int indR = 0;
-        int indRSL = 0;
-        if (left.length == 0) {
-            while (indR < right.length) {
-                System.out.println(indR);
-                rsl[indRSL++] = right[indR++];
-            }
-        }
-        if (right.length == 0) {
-            while (indL < left.length) {
-                rsl[indRSL++] = left[indL++];
-            }
-        }
-        while (indL < left.length && indR < right.length) {
-            rsl[indRSL++] = left[indL] < right[indR] ?  left[indL++] : right[indR++];
-            if (indRSL >= left.length || indRSL >= right.length) {
-                while (indL < left.length) {
-                    rsl[indRSL++] = left[indL++];
-                }
-                while (indR < right.length) {
-                    rsl[indRSL++] = right[indR++];
-                }
+        int leftPoint = 0;
+        int rightPoint = 0;
+        int rslPoint = 0;
+        while (leftPoint + rightPoint != rsl.length) {
+            if (leftPoint != left.length && rightPoint != right.length) {
+                rsl[rslPoint++] = left[leftPoint] < right[rightPoint] ? left[leftPoint++] : right[rightPoint++];
+            } else if (leftPoint != left.length) {
+                rsl[rslPoint++] = left[leftPoint++];
+            } else {
+                rsl[rslPoint++] = right[rightPoint++];
             }
         }
         return rsl;
+    }
+
+    public static void main(String[] args) {
+        int[] rsl = merge(
+                new int[] {1, 3, 5},
+                new int[] {2, 4}
+        );
+        System.out.println(Arrays.toString(rsl));
     }
 }
